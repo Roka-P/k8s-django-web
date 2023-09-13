15f53bf08a0792a72f705c101f9dd6e8296592c0
Roka Park <74800708+Roka-P@users.noreply.github.com>
2023-09-13 13:39:39 +0900

    Update apps.py

--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HomeConfig(AppConfig):
+class ThemeMaterialKitConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'home'
+    name = 'theme_material_kit'
